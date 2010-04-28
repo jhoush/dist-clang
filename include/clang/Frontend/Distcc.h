e107b4fbab69978b78e234a2574f3d2b263322de
Michael Miller <mike@resnet-45-230.dorm.utexas.edu>
2010-04-27 22:15:08 -0500

    Add in initial zmq support. Untested code(but it compiles).

--- include/clang/Frontend/Distcc.h
+++ include/clang/Frontend/Distcc.h
@@ -14,32 +14,79 @@
 
 #include <sys/un.h>
 
+#include <queue>
+#include <map>
+
+// FIXME: Stop using zmq
+#include <zmq.hpp>
+
 using namespace llvm;
 
 namespace clang {
 	struct DistccClient{
 		int fd; // fd to talk to client
 		std::vector<std::string> args;
-		bool dataSent; //to slave
-		bool dataReceived; //from slave
 	};
 	
 class Distcc {
 private:
+	//Pipeline:
+	
+	//1. In AcceptThread, a new client is created and put onto
+	//the clientsAwaitingDistribution queue
+	
+	//2. The preprocessThread pulls the request off the queue.
+	//It then preprocesses the file, and assigns it a unique ID.
+	//Then, it puts it into the clientsAwaitingObjectCode map
+	//(which maps a unique ID to a client).
+	
+	//3. ReceiveThread is constantly looking for slaves to
+	//receive data from. When it finds a message from the
+	//slave with object code/diags, it recieves the object
+	//code and diags from the slave, writes the object code
+	//to disk, and sends the diags back to the slave.
+	
+	//FIXME: Send preproc diags back as well as post-preproc diags!
+	
+	
 	// Server-side methods/vars
-	std::vector<DistccClient> localClients; //holds fds of local clients
-	sys::Mutex localClientsMutex; //mutex protecting localClients vector
+	std::queue<DistccClient> clientsAwaitingDistribution; // Holds files which need to be distributed
+	sys::Mutex clientsAwaitingDistributionMutex; //mutex protecting filesAwaitingDistribution queue
+	
+	
+	// Mapping from unique identifier -> client
+	// unique ID is used to quickly identify file when object code/diags come back
+	// Only files whose source have been sent out to a slave are stored in this
+	std::map<uint64_t, DistccClient> clientsAwaitingObjectCode;
+	sys::Mutex clientsAwaitingObjectCodeMutex;
+	
+	uint64_t counter; // Used to ensure same unique idenifier doesn't get used twice
+	sys::Mutex counterMutex; // FIXME: Use atomic increment instead of mutex??
+	
+	int currentSlave; // Used so we can round-robin slaves
+	
+	std::vector<zmq::socket_t*> slaves; //holds sockets connected to slaves
+	void *ConnectToSlaves();
+	
+	std::map<uint64_t,DistccClient> files;
+	
+	
+	zmq::context_t zmqContext;
 	
 	pthread_t acceptThread;
 	pthread_t preprocessThread;
+	pthread_t receiveThread;
+	
 	int acceptSocket;
 	
 	void *AcceptThread();
 	void *PreprocessThread();
+	void *ReceiveThread();
 	
 	//Boostrapping functions for pthreads
 	static void *pthread_AcceptThread(void *ctx);
 	static void *pthread_PreprocessThread(void *ctx);
+	static void *pthread_ReceiveThread(void *ctx);
 	
 	void startServer(struct sockaddr_un &addr);
 
@@ -55,7 +102,7 @@ private:
 
 	
 public:
-	Distcc(CompilerInstance &instance);
+	Distcc(CompilerInstance &instance);	
 	~Distcc();
 
 };
--- lib/Frontend/Distcc.cpp
+++ lib/Frontend/Distcc.cpp
@@ -19,6 +19,7 @@
 #include "clang/Lex/Preprocessor.h"
 
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/MemoryBuffer.h"
 
 // FIXME: Replace UNIX-specific operations with system-agnostc ones
 #include <pthread.h>
@@ -30,14 +31,23 @@
 #include <pthread.h>
 #include <limits.h>
 
+// FIXME: Stop using zmq
+#undef error_t
+#include <zmq.hpp>
+
+//FIXME: Handle sending return codes to Client!
 
 using namespace clang;
 
 //Note: http://beej.us/guide/bgipc/ was used as a reference when making socket code
 
-Distcc::Distcc(CompilerInstance &instance){
+Distcc::Distcc(CompilerInstance &instance)
+: //FIXME: Determine proper value for app/io threads(respectively)
+zmqContext(2, 2)
+{
 
-	
+	counter = 0;
+	currentSlave = 0;
 	this->CI = &instance;
 	const char *socketPath = "/tmp/clangSocket";
 	
@@ -49,7 +59,6 @@ Distcc::Distcc(CompilerInstance &instance){
 
 	
 	
-	
 	// Attempt to connect to server
 	struct sockaddr_un remote;
 	remote.sun_family = AF_UNIX;
@@ -58,16 +67,10 @@ Distcc::Distcc(CompilerInstance &instance){
 	if ((r=connect(serverFd, (struct sockaddr *)&remote, sizeof(remote))) == -1){
 		//Didn't connect, so we have to fork!
 		if((r=fork())==0){
-			llvm::errs() << "Child\n";
-			
 			//Child(server)
 			startServer(remote);	
-			llvm::errs() << "Returned from server\n";
-
 		}
 		else if(r>0){
-			llvm::errs() << "Parent\n";
-
 			//Parent(client)
 			// Sleep until (approx) when socket is ready,
 			//
@@ -80,20 +83,13 @@ Distcc::Distcc(CompilerInstance &instance){
 			//FIXME: Is 100 us the right amount of time to sleep?
 			while (connect(serverFd, (struct sockaddr *)&remote, sizeof(remote)) == -1)
 				usleep(100); 
-			llvm::errs() << "ParentConnected\n";
-
 			startClient();
 			return;
 		}
 		else if(r<0){
 			llvm::errs() << "Error forking\n";
-
+			return;
 		}
-	}
-	else {
-		// TODO: Remove this
-		llvm::errs() << "Ret code" << r <<  "\n";
-
 	}
 	
 	//Connected to server, just start client
@@ -117,98 +113,124 @@ void Distcc::startServer(struct sockaddr_un &addr){
 	// we never drop connections.
 	if(listen(acceptSocket, 10)<0){
 		llvm::errs() << "Error listening to socket(startServer)\n";
-
 		close(acceptSocket);
 	}
 	
+	
+	ConnectToSlaves(); // This is OK to be nonthreaded b/c connections are created async by zmq
+
 	// Start server to accept connections
 
 	if(pthread_create(&acceptThread, NULL, pthread_AcceptThread, this)<0){
 		llvm::errs() << "Error creating AcceptThread\n";
-
 		close(acceptSocket);
 	}
 	
 	if(pthread_create(&preprocessThread, NULL, pthread_PreprocessThread, this)<0){
 		llvm::errs() << "Error creating preprocessor thread\n";
-
 		close(acceptSocket);
 	}
-
+	
+	if(pthread_create(&receiveThread, NULL, pthread_ReceiveThread, this)<0){
+		llvm::errs() << "Error creating preprocessor thread\n";
+		close(acceptSocket);
+	}
+	
 	pthread_exit(NULL);
 }
+
+// This function's job is to preprocess files and send files to slaves
+
 void *Distcc::PreprocessThread(){
 	//FIXME: Timeout after some period
 	FileManager fm; // Outside of loop so we have consistent cache
 	while(1){
-		// Go through each client, check if we've sent out the args for the client yet
-		std::vector<DistccClient>::iterator itr;
-		localClientsMutex.acquire();
-		for(itr = localClients.begin(); itr != localClients.end(); ++itr){
-			if((*itr).dataSent == false){
-				//If no data sent, preprocess, and send out
-				std::string preprocessedSource;
-				llvm::raw_string_ostream *OS = new llvm::raw_string_ostream(preprocessedSource);
-				
-				
-				TextDiagnosticBuffer DiagsBuffer;
-				CompilerInvocation Invocation;
-				Diagnostic Diags(&DiagsBuffer);
-				int length;
-				
-				
-				std::vector<std::string> args = (*itr).args;
-				llvm::SmallVector<const char *, 32> argAddresses;
-				for(unsigned i=0;i<args.size();i++){
-					argAddresses.push_back(args[i].c_str());
-					llvm::errs() << args[i] << "\n";
-				}
-				
-				CompilerInvocation::CreateFromArgs(Invocation, (const char **)argAddresses.begin(),
-												   (const char **)argAddresses.end(), Diags);
-				
-				
-				TargetInfo *Target = TargetInfo::CreateTargetInfo(Diags, Invocation.getTargetOpts());
-				SourceManager SourceMan(Diags);
-				HeaderSearch hs(fm);
-				
-				
-				// FIXME: Is it bad to assume that first arg == filename
-				// FIXME: Note, this means we can't take stdin as input("-")
-				const FileEntry *file = fm.getFile(args[0].c_str()); 
-				SourceMan.createMainFileID(file, SourceLocation());
-				
-				Preprocessor localPreprocessor(Diags, Invocation.getLangOpts(), *Target, SourceMan, hs);
-				llvm::errs() << "Preprocessor created\n";
+		
+		clientsAwaitingDistributionMutex.acquire(); 
+		//Pop file to send off queue
+		DistccClient client = clientsAwaitingDistribution.front();
+		clientsAwaitingDistribution.pop();
+		clientsAwaitingDistributionMutex.release();
 
-
-				DoPrintPreprocessedInput(localPreprocessor, OS,
-										 Invocation.getPreprocessorOutputOpts());
-				OS->flush();
-				llvm::errs() << "Preprocessed source\n" << preprocessedSource << "\n";
-
-
-				delete OS;
-				const char *data = preprocessedSource.c_str();
-				
-				// FIXME: Actually send work out to nodes
-
-				// Send back length of data
-				length = preprocessedSource.length();
-				if(send((*itr).fd, &length, sizeof(length), 0)<(int)sizeof(length)){
-					llvm::errs() << "Error sending length of data to dumb makefile-spawned process\n";
-				}
-				
-				//Send back actual data
-				if(send((*itr).fd, data, length, 0)<(int)sizeof(length)){
-					llvm::errs() << "Error sending actual data back to dumb makefile-spawned process\n";
-				}
-				
-				(*itr).dataSent = true;
-			}
+		//If no data sent, preprocess, and send out
+		std::string preprocessedSource;
+		llvm::raw_string_ostream *OS = new llvm::raw_string_ostream(preprocessedSource);
+		
+		
+		TextDiagnosticBuffer DiagsBuffer;
+		CompilerInvocation Invocation;
+		Diagnostic Diags(&DiagsBuffer);
+		
+		
+		std::vector<std::string> args = client.args;
+		llvm::SmallVector<const char *, 32> argAddresses;
+		for(unsigned i=0;i<args.size();i++){
+			argAddresses.push_back(args[i].c_str());
+			llvm::errs() << args[i] << "\n";
 		}
-		localClientsMutex.release();
-		usleep(50);
+		
+		CompilerInvocation::CreateFromArgs(Invocation, (const char **)argAddresses.begin(),
+										   (const char **)argAddresses.end(), Diags);
+		
+		TargetInfo *Target = TargetInfo::CreateTargetInfo(Diags, Invocation.getTargetOpts());
+		SourceManager SourceMan(Diags);
+		HeaderSearch hs(fm);
+
+		
+		// FIXME: Note, this means we can't take stdin as input("-")
+		const FileEntry *file = fm.getFile(args[args.size()-2].c_str()); 
+		SourceMan.createMainFileID(file, SourceLocation());
+		Preprocessor localPreprocessor(Diags, Invocation.getLangOpts(), *Target, SourceMan, hs);
+
+
+		DoPrintPreprocessedInput(localPreprocessor, OS,
+								 Invocation.getPreprocessorOutputOpts());
+		OS->flush();
+		delete OS;
+		
+		while(slaves.empty())
+			usleep(50); // Wait until we have at least 1 slave to send to
+		
+		int argLen;
+		char *serializedArgs = serializeArgVector(client.args, argLen);
+		
+		int preprocessedSourceLen = preprocessedSource.length() + 1; //For null byte
+		
+		int totalLen = preprocessedSourceLen + argLen + 4 + 8; /*
+																length of preprocessed source + 
+																argument length +
+																length of integer which holds length of args +
+																length of UID +
+																*/
+		
+		// FIXME: Atomic increment instead of mutex?
+		counterMutex.acquire();
+		uint64_t uniqueID = counter++;
+		counterMutex.release();
+		
+		// Compose message
+		zmq::message_t msg(totalLen);
+		char *offset = (char *)msg.data();
+		memcpy(offset, &uniqueID, sizeof(uniqueID)); //Copy uniqueid into message 
+		offset += sizeof(uniqueID);
+		memcpy(offset, &argLen, sizeof(argLen)); //Copy arg length into message
+		offset += sizeof(argLen);
+		memcpy(offset, serializedArgs, argLen); //Copy args into message
+		offset += argLen;
+		memcpy(offset, preprocessedSource.c_str(), preprocessedSourceLen);
+		offset += preprocessedSourceLen;
+		
+		slaves[currentSlave++]->send(msg);
+		
+		
+		clientsAwaitingObjectCodeMutex.acquire();
+		clientsAwaitingObjectCode.insert(std::pair<uint64_t,DistccClient>(uniqueID, client));
+		clientsAwaitingObjectCodeMutex.release();
+
+		
+		free(serializedArgs);		
+		if(clientsAwaitingDistribution.empty()) //Shouldn't need mutex here(worst case, we sleep)
+			usleep(50); //Sleep if there's nothing to do for now
 	}
 }
 //Args stored as <arg1>\0<arg2>\0<arg3>\0......<argN>\0
@@ -286,7 +308,6 @@ void *Distcc::AcceptThread(){
 			continue;
 		}
 		
-		llvm::errs() << "Finished AcceptThread accept\n";
 
 		
 		//Read the serialized string
@@ -303,17 +324,14 @@ void *Distcc::AcceptThread(){
 		DistccClient client;
 		client.fd = fd2;
 		client.args = deserializeArgVector(argString, sizeOfString);
-		client.dataSent = false;
-		client.dataReceived = false;
+
 		
 		free(argString);
 		
 		//Stick the client onto the end of the list
-		localClientsMutex.acquire();
-		localClients.push_back(client);
-		llvm::errs() << "Pushed\n";
-
-		localClientsMutex.release();
+		clientsAwaitingDistributionMutex.acquire();
+		clientsAwaitingDistribution.push(client);
+		clientsAwaitingDistributionMutex.release();
 	}
 	return NULL; //Should never hit
 }
@@ -339,23 +357,24 @@ void Distcc::startClient(){
 	
 	SourceManager &sm = CI->getSourceManager();
 	const FileEntry *fe = sm.getFileEntryForID(sm.getMainFileID());
-	std::string filename(fe->getName());
-	args.push_back(filename); // Push filename onto end	of args
+	const char *relativeFileName = fe->getName();
 	
-	char path[FILENAME_MAX];
+	char path[PATH_MAX];
 	if(path==NULL){
 		llvm::errs() << "Error malloc'ing space for path\n";
 		return;
 	}
-	if(getcwd(path, sizeof(path))==NULL){
+	if(realpath(relativeFileName, path)==NULL){
 		llvm::errs() << "Error getcwd'ing\n";
 		return;
 	}
 	std::string pathString(path);
 	args.push_back(pathString); // Push working dir onto end of args
 	
-	llvm::errs() << "File is " << filename <<  "\n";
+	llvm::errs() << "File is " << relativeFileName <<  "\n";
 	
+	//Send output filename to server
+	args.push_back(CI->getFrontendOpts().OutputFile);
 	
 	// Send length of args
 	int lengthOfArgs = 0;
@@ -377,36 +396,123 @@ void Distcc::startClient(){
 	free(serializedArgs);
 	
 	
-	// FXIXME: Recieve diags
-	llvm::errs() << "Recievining length\n";
-
-	// FIXME: Recieve preprocessed source(remove after done)
-	int sourceLength = 0;
-	int lengthRead;
-	if((lengthRead=recv(serverFd, &sourceLength, sizeof(sourceLength), MSG_WAITALL))<(int)sizeof(sourceLength)){
-		llvm::errs() << "Error recieving length of source " << lengthRead << "\n" << "Errno: " << errno << "\n";
-		exit(1); // TODO: Remove this
+	int lengthRead = 0;
+	int sizeOfDiags = 0;
+	// Recieve length of diags
+	if((lengthRead = recv(serverFd, &sizeOfDiags, sizeof(sizeOfDiags), MSG_WAITALL))<(int)sizeof(sizeOfDiags)){
+		llvm::errs() << "Error recieving length of diags from server " << lengthRead << "\n";
+		close(serverFd);
 		return;
 	}
-	llvm::errs() << "Recieving source\n";
-
-	char *source = (char *)malloc(sizeof(char)*sourceLength+1);
-	source[sourceLength] = '\0'; // Tack on null byte
-	if(recv(serverFd, source, sourceLength, MSG_WAITALL)<(int)sourceLength){
-		llvm::errs() << "Error recieving source itself\n";
+	
+	
+	// Recieve diags themselves
+	char *diags = (char *)malloc(sizeof(char)*sizeOfDiags);
+	if((lengthRead = recv(serverFd, diags, sizeOfDiags, MSG_WAITALL))<(int)sizeOfDiags){
+		llvm::errs() << "Error recieving diags from server " << lengthRead << "\n";
+		close(serverFd);
+		free(diags);
+		return;
 	}
-	llvm::errs() << "Done recieving source\n";
-
-	std::string sourceString(source);
-	free(source);
-	llvm::errs() << "Source string: " << sourceString << "\n";
-
-	// FIXME: Recieve object code
-
+	// FIXME: Do something other than print diags?
+	llvm::errs() << diags;
+	
+	//FIXME: Use proper exit code(Make will continue even if file has errors!)
+	
+	free(diags);
 }
 
-
-
+void *Distcc::ConnectToSlaves(){
+	//Setup context
+	
+	//FIXME: Remove hardcoding!
+	llvm::errs() << "Constructing membuf\n";
+	llvm::errs().flush();
+	MemoryBuffer *Buf = MemoryBuffer::getFile("/Volumes/Data/Users/mike/Desktop/config.txt");
+	llvm::errs() << "Finished constructing membuf\n";
+	llvm::errs().flush();
+	const char *start = Buf->getBufferStart();	
+	const char *end = Buf->getBufferEnd();
+	
+	
+	char *startChar = (char*)start;
+	char *curChar = (char *)start;
+	for ( ; curChar < end ; ++curChar){
+		//FIXME: Handle windows strings(\r\n)
+		//FIXME: File must end with newline!
+		//FIXME: Add support for comments(lines beginning with #)
+		//FIXME: Remove whitespace, and ignore empty lines
+		if(*curChar == '\n'){
+			std::string addr(startChar, curChar - startChar);
+			startChar = curChar+1;
+			
+			zmq::socket_t *s = new zmq::socket_t(zmqContext,ZMQ_P2P);
+			//NOTE: This is async connect, so it is fast, but the connection is not guaranteed to succeed!
+			s->connect(addr.c_str());
+			slaves.push_back(s);
+		}
+	}
+	llvm::errs().flush();
+}
+//Recieve messages from slaves continuously, and write out object code as relevant
+void *Distcc::ReceiveThread(){
+	while(1){
+		int slaveSize = slaves.size();
+		for(int i=0;i<slaveSize;i++){
+			zmq::message_t msg; //FIXME: Move this out of the loop to reuse message?
+			if(slaves[i]->recv(&msg, ZMQ_NOBLOCK)<0){
+				if(errno != EAGAIN){
+					llvm::errs() << "Error reading from socket(not EAGAIN): " << errno << "\n";
+				}
+				continue; //No data available on slave, just try next one
+			}
+			//Process message
+			int messageLen = msg.size();
+			char *offset = (char *)msg.data();
+			uint64_t uniqueID = *(uint64_t *)offset;
+			offset += sizeof(uint64_t);
+			uint32_t diagLen = *(uint32_t *)offset;
+			offset += sizeof(uint32_t);
+			char *diags  = (char*)offset;
+			offset += diagLen;
+			const char *objCode = (const char*)offset;
+			int objLen = messageLen - (offset - (char*)msg.data());
+			
+			DistccClient client = clientsAwaitingObjectCode[uniqueID];
+			
+			//Write object code to disk
+			std::string errorInfo;
+			llvm::raw_fd_ostream outputStream(client.args[client.args.size()-1].c_str(), errorInfo);
+			outputStream.write(objCode, objLen);
+			outputStream.close();
+			if(errorInfo.size() > 0)
+				llvm::errs() << errorInfo << "\n";
+			
+			//Send length of diags to client, blocking
+			if(send(client.fd, &diagLen, sizeof(diagLen), 0)<0){
+				llvm::errs() << "Error sending diags back to client\n";
+				//FIXME: Handle this error better
+			}
+			
+			//Send diags to client, blocking
+			//FIXME: Async?
+			if(send(client.fd, diags, diagLen, 0)<0){
+				llvm::errs() << "Error sending diags back to client\n";
+				//FIXME: Handle this error better
+			}
+			
+			//Close connection to client
+			close(client.fd);
+			
+			// Take out client from 
+			clientsAwaitingObjectCodeMutex.acquire();
+			clientsAwaitingObjectCode.erase(clientsAwaitingObjectCode.find(uniqueID));
+			clientsAwaitingObjectCodeMutex.release();
+			
+		}
+		
+	}
+}
 // Static pthread helper method
 void *Distcc::pthread_AcceptThread(void *ctx){
 	return ((Distcc *)ctx)->AcceptThread();
@@ -414,6 +520,10 @@ void *Distcc::pthread_AcceptThread(void *ctx){
 void *Distcc::pthread_PreprocessThread(void *ctx){
 	return ((Distcc *)ctx)->PreprocessThread();
 }
+void *Distcc::pthread_ReceiveThread(void *ctx){
+	return ((Distcc *)ctx)->ReceiveThread();
+}
+
 Distcc::~Distcc(){
 
 }
